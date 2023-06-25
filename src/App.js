c145f50e86df177c05dd7ec2b81d96f2fb6435c6
rio120140025 <rio.120140025@student.itera.ac.id>
2023-06-25 20:59:54 +0700

    formating and add dashboard header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import Login from "./components/login-page/login";
-import { RoleRegister, RoleRegisterDPL, RoleRegisterDosen, RoleRegisterMahasiswa } from "./components/register-page/register";
-import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Box } from "@chakra-ui/layout";
+
+import "./App.css";
+
+// import Login from "./components/login-page/login";
+// import {
+//   RoleRegister,
+//   RoleRegisterDPL,
+//   RoleRegisterDosen,
+//   RoleRegisterMahasiswa,
+// } from "./components/register-page/register";
+
+import Dashboard from "./components/dashboard-page/dashboard";
 
 function App() {
   return (
     <BrowserRouter>
-      <Box
+      <Dashboard />
+      {/* <Box
         bgImage={"background.jpg"}
         height={"100vh"}
         width={"100vw"}
@@ -19,11 +29,19 @@ function App() {
         <Routes>
           <Route exact path="/" Component={Login} />
           <Route exact path="/register" Component={RoleRegister} />
-          <Route exact path="/register-Mahasiswa" Component={RoleRegisterMahasiswa} />
+          <Route
+            exact
+            path="/register-Mahasiswa"
+            Component={RoleRegisterMahasiswa}
+          />
           <Route exact path="/register-Dosen" Component={RoleRegisterDosen} />
-          <Route exact path="/register-Dosen Pembimbing" Component={RoleRegisterDPL} />
+          <Route
+            exact
+            path="/register-Dosen Pembimbing"
+            Component={RoleRegisterDPL}
+          />
         </Routes>
-      </Box>
+      </Box> */}
     </BrowserRouter>
   );
 }
