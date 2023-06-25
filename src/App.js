44d945cea74c6246d31d9a1842bdb9228ce9f82f
muhammadhaikalfauzananda <muhammad.120140168@student.itera.ac.id>
2023-06-25 12:38:25 +0700

    react route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/login-page/login";
+import Register from './components/Register-page/Register'
 import "./App.css";
 
 function App() {
   return (
     <div>
-      <Login />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Login />}></Route>
+          <Route path='/Register' element={<Register />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
